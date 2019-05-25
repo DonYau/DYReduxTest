920154ac870b6e524cd95c5972fb6a498c2cfa32
DonYau <DonYau@126.com>
2019-05-25 13:42:09 +0800

    redux简介

---
+++ redux-1/src/index.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+
+const addAction = (num1,num2) => {
+    return ({
+        type: 'ADD',
+        num1,
+        num2,
+    })
+}
+
+const addReducer = (state,action) => {
+    if(action.type === 'ADD') {
+        return ({
+            result: action.num1 + action.num2
+        })
+    }
+}
+
+const store = createStore(addReducer,{},composeWithDevTools())
+
+setInterval(() => {
+    store.dispatch(addAction(parseInt(Math.random() * 10) ,2))
+},2000)
+
